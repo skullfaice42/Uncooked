4e387c285cb6ddd573ab597b7c43d4e8697f20fc
skullfaice42 <skullfart875@gmail.com>
2025-01-12 14:03:55 -0800

    push
    testing on my phone

--- loading-screen/loading2.js
+++ loading-screen/loading2.js
@@ -7,6 +7,6 @@ document.addEventListener("DOMContentLoaded", function() { //domcontentloaded en
         loading.style.opacity = 0;
         setTimeout(function(){
             loading.style.display = "none"},4000); //had to update because 2 seconds wasent long enough
-    }, 2000);
+    }, 4000);
 });
 ;
